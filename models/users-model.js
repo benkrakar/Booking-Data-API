780e24b8fbef9a145cf7b640ff3c895560baf7d4
benkrakar <benkrakarwalid44@gmail.com>
2022-01-15 23:37:57 +0100

    add validator and passwordconfirmation

--- models/users-model.js
+++ models/users-model.js
@@ -1,3 +1,5 @@
+import validator from 'validator';
+
 const usersShema = (mongoose) =>
   new mongoose.Schema({
     name: {
@@ -7,17 +9,34 @@ const usersShema = (mongoose) =>
     },
     email: {
       type: String,
+      validate: [validator.isEmail, 'Please enter a valid email'],
       required: [true, 'user must have a email'],
       unique: true,
     },
     password: {
       type: String,
       required: [true, 'user must have a password'],
+      minlength: 8,
+    },
+    passwordConfirm: {
+      type: String,
+      required: [true, 'please confirm your password'],
+      minlength: 8,
+      validate: {
+        validator: function (pwd) {
+          return pwd === this.password;
+        },
+        messages: 'Passwords do not match!',
+      },
     },
     role: {
       type: String,
       required: false,
       default: 'user',
+    },
+    created_at: {
+      type: Date,
+      default: Date.now(),
     },
   });
 export default usersShema;
