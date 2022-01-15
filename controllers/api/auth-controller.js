1e9d261a0442c50c9e9f5049d4ffa30b0f81072f
benkrakar <benkrakarwalid44@gmail.com>
2022-01-15 23:38:06 +0100

    register new user

--- controllers/api/auth-controller.js
+++ controllers/api/auth-controller.js
@@ -1,10 +1,25 @@
+import models from '../../models/index.js';
+
 class AuthController {
   async login(req, res) {
     console.log('hi from controller 👋👋');
   }
 
   async register(req, res) {
-    console.log('hi from controller 👋👋');
+    try {
+      const newUser = await models.users.create(req.body);
+      res.status(202).json({
+        status: 'success',
+        data: {
+          user: newUser,
+        },
+      });
+    } catch (err) {
+      res.status(400).json({
+        status: 'fail',
+        message: err,
+      });
+    }
   }
 }
 
