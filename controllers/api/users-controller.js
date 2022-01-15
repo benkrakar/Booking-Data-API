96722619663cf090cdb2143b1536856cfdf8e387
benkrakar <benkrakarwalid44@gmail.com>
2022-01-15 16:36:57 +0100

    create users controller

---
+++ controllers/api/users-controller.js
@@ -0,0 +1,23 @@
+class UsersController {
+  async getUser(req, res) {
+    console.log('hi from controller 👋👋');
+  }
+
+  async getUsers(req, res) {
+    console.log('hi from controller 👋👋');
+  }
+
+  async createUser(req, res) {
+    console.log('hi from controller 👋👋');
+  }
+
+  async updateUser(req, res) {
+    console.log('hi from controller 👋👋');
+  }
+
+  async deleteUser(req, res) {
+    console.log('hi from controller 👋👋');
+  }
+}
+
+export default new UsersController();
