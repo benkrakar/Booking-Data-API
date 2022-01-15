dbf06be0f879beda320620ccb46fa8787e450a06
benkrakar <benkrakarwalid44@gmail.com>
2022-01-15 13:56:29 +0100

    connection with mongodb

--- server/index.js
+++ server/index.js
@@ -1,20 +1,37 @@
 import express from 'express';
+import Mongoose from 'mongoose';
+import 'dotenv/config';
 import Router from '../router/index.js';
 
 class Server {
   constructor(port) {
     this.port = port;
     this.router = Router;
+    this.mongoose = Mongoose;
+    this.db = process.env.DATABASE_LOCAL;
     this.app = express();
   }
 
   start() {
     this._setupRoutes();
+    this._setupConnection();
     this._listen();
   }
 
   _setupRoutes() {
     this.router.create(this.app);
+  }
+
+  _setupConnection() {
+    this.mongoose
+      .connect(this.db, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+      })
+      .then(() => {
+        console.log('connection 👌');
+      });
   }
 
   _listen() {
